6a2518d36331873a9c713e0ee9753e4b2f40c8f1
Gabriel <gabriel@dinu.tech>
2022-12-22 13:58:57 +0100

    :rocket: palindromLinkedList challenge

--- source/src/Main.java
+++ source/src/Main.java
@@ -8,15 +8,11 @@ public class Main {
 //    1. ChallengeClass.function(input)
 //    2. Print the challenge
 
-    static ListNode n4 = new ListNode(1);
-    static ListNode n3 = new ListNode(2, n4);
-    static ListNode n2 = new ListNode(2, n3);
-    static ListNode n1 = new ListNode(1, n2);
+
 
 
     public static void main(String[] args) {
 
-        System.out.println(palindromeLinkedList.isPalindrome(n1));
 
     }
 }
--- source/src/challenges/palindromeLinkedList/palindromeLinkedList.java
+++ source/src/challenges/palindromeLinkedList/palindromeLinkedList.java
@@ -3,6 +3,12 @@ package challenges.palindromeLinkedList;
 import java.util.ArrayList;
 import java.util.Objects;
 
+// Insert Data Structure in Main.java and start the function with n1
+//static ListNode n4 = new ListNode(1);
+//static ListNode n3 = new ListNode(2, n4);
+//static ListNode n2 = new ListNode(2, n3);
+//static ListNode n1 = new ListNode(1, n2);
+
 public class palindromeLinkedList {
 
     public static boolean isPalindrome(ListNode head){
